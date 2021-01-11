555eb3349837a51c104972c15463dbe2c8eaa003
Hilary B <hilarybontz@gmail.com>
2021-01-10 18:17:54 -0800

    Updates

---
+++ precache-manifest.9ef4b3ec485386d4a10b17b537bd22fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91169386b18663319342dc3e967ffdf",
+    "url": "/would_you_rather/index.html"
+  },
+  {
+    "revision": "06bcac54f72bec1eb3d9",
+    "url": "/would_you_rather/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "061449a0790eaceb6913",
+    "url": "/would_you_rather/static/css/main.113904e2.chunk.css"
+  },
+  {
+    "revision": "06bcac54f72bec1eb3d9",
+    "url": "/would_you_rather/static/js/2.8d4b8b6c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/would_you_rather/static/js/2.8d4b8b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061449a0790eaceb6913",
+    "url": "/would_you_rather/static/js/main.ab1b49cf.chunk.js"
+  },
+  {
+    "revision": "73f293a5ada0c74b0bdb",
+    "url": "/would_you_rather/static/js/runtime-main.cb375b74.js"
+  }
+]);
